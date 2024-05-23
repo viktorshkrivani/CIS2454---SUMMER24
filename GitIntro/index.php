453d92ac60f4800859a13706e9b226f7b9b9d28f
viktorshkrivani <shkrivani.viktor@student.oaklandcc.edu>
2024-05-23 01:27:28 -0400

    Update index.php

--- GitIntro/index.php
+++ GitIntro/index.php
@@ -10,7 +10,13 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
     </head>
     <body>
         <?php
-        echo "Hello World!";
+        echo 'Hello World';
+        echo '<br>Name: Viktor Shkrivani, Software Engineering</br>';
+        echo '<br>I am taking the class, because I really want to finish the  Associate Degree.</br>';
+        echo '<br>Previous Programming languages I can include, Java, C++, HTML, CSS and more.</br>';
+        echo '<br>I have many hobbies but, the last one that I started practicin mori si golf.</br>';
+        echo '<br>My plan fo the fall is starting my own affiliate website.</br>';
+        echo '<br>I cannot leave without coffe, and CHIPOTLE.</br>';
         ?>
     </body>
 </html>
